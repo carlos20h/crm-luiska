9b8db7ef98296a490b9b32e70931cc1a871a7154
carlos20h <lcarlos.hermosillo@gmail.com>
2025-08-26 12:29:25 -0600

    chore: setup Next.js project with Tailwind and ESLint

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-24">
+      <h1 className="text-2xl font-bold">Hello World</h1>
+    </main>
+  );
+}
